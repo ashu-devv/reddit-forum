2d375f080217d4731c03644001a884dc291269f0
Ashutosh Shukla <ashutosh6471@gmail.com>
2024-07-06 23:06:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ const {serverConfig,Logger} = require('./config');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT ||6000, () => {
     console.log(`Successfully started the server at port ${serverConfig.PORT}`);
     Logger.info('Successfully Started The Server', 'root', {});
 }) 
