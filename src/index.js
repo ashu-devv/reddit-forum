91fb93819b73c280d49d01eb7d23a1b0e31ee242
Ashutosh Shukla <ashutosh6471@gmail.com>
2024-07-06 23:04:01 +0530

    Installed nodemon and related packages

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+const postRoutes = require('./routes/post-routes');
+const bodyParser = require('body-parser');
+const {serverConfig,Logger} = require('./config');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+
+app.listen(serverConfig.PORT, () => {
+    console.log(`Successfully started the server at port ${serverConfig.PORT}`);
+    Logger.info('Successfully Started The Server', 'root', {});
+}) 
